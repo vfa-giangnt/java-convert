7bc69050ddb2e98a32906459916f5432fa38f92b
vfa.giangnt <giangnt@vitalify.asia>
2020-06-26 12:00:42 +0700

    Java I/O: Delete file

--- src/vitalify/asia/Main.java
+++ src/vitalify/asia/Main.java
@@ -1,8 +1,35 @@
 package vitalify.asia;
 
+import java.io.File;
+import java.io.IOException;
+
 public class Main {
     public static void main(String[] args) {
-	// write your code here
+        File file = new File("./myfile.txt");
+        createNewFile(file);
 
+        try {
+            if (file.delete()) {
+                System.out.println(file.getName() + " is deleted.");
+            }
+        } catch (Exception e) {
+            System.out.println("Exception occurred");
+            e.printStackTrace();
+        }
+    }
+
+    private static void createNewFile(File file) {
+        try {
+            boolean fvar = file.createNewFile();
+            if (fvar) {
+                System.out.println("File has been created successfully");
+            } else {
+                System.out
+                        .println("File already present at the specific location");
+            }
+        } catch (IOException exception) {
+            System.out.println("Exception Occurred");
+            exception.printStackTrace();
+        }
     }
 }
