454cdab8351ec2cca01cf9ee2c991a7378c8f594
vfa.giangnt <giangnt@vitalify.asia>
2020-06-26 13:24:07 +0700

    Java Multithreading: with Thread

--- src/vitalify/asia/Main.java
+++ src/vitalify/asia/Main.java
@@ -2,7 +2,49 @@ package vitalify.asia;
 
 public class Main {
     public static void main(String[] args) {
-	// write your code here
+        /*MultithreadingDemo demo = new MultithreadingDemo();
+        demo.start();*/
 
+        Count count = new Count();
+        try {
+            while (count.isAlive()) {
+                System.out.println("Main thread will be alive till the child thread is live");
+                Thread.sleep(1500);
+            }
+        } catch (InterruptedException e) {
+            System.out.println("Main thread interrupted");
+            e.printStackTrace();
+        }
+        System.out.println("Main thread run over");
+    }
+
+    public static class MultithreadingDemo extends Thread {
+        @Override
+        public void run() {
+            System.out.println("My thread is in running state");
+        }
+    }
+
+    public static class Count extends Thread {
+
+        public Count() {
+            super("My extending thread");
+            System.out.println("Thread created => " + this);
+            start();
+        }
+
+        @Override
+        public void run() {
+            try {
+                for (int i = 0; i < 10; i++) {
+                    System.out.println("Printing count " + i);
+                    Thread.sleep(1000);
+                }
+            } catch (InterruptedException e) {
+                System.out.println("My thread interrupted");
+                e.printStackTrace();
+            }
+            System.out.println("Thread run is over");
+        }
     }
 }
