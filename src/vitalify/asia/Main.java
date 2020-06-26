ec784a9a12626535597e255d4b26981057e36dfe
vfa.giangnt <giangnt@vitalify.asia>
2020-06-26 11:19:24 +0700

    Java I/O: Read file with BufferInputStream

--- src/vitalify/asia/Main.java
+++ src/vitalify/asia/Main.java
@@ -1,8 +1,36 @@
 package vitalify.asia;
 
+import java.io.BufferedInputStream;
+import java.io.File;
+import java.io.FileInputStream;
+import java.io.IOException;
+
 public class Main {
     public static void main(String[] args) {
-	// write your code here
+        File file = new File("./myfile.txt");
+        BufferedInputStream bufferedInputStream = null;
+        FileInputStream fileInputStream = null;
 
+        try {
+            fileInputStream = new FileInputStream(file);
+            bufferedInputStream = new BufferedInputStream(fileInputStream);
+
+            while (bufferedInputStream.available() > 0) {
+                System.out.print((char) bufferedInputStream.read());
+            }
+        } catch (IOException e) {
+            System.out.println("I/O Exception: " + e);
+            e.printStackTrace();
+        } finally {
+            try {
+                if (bufferedInputStream != null) {
+                    fileInputStream.close();
+                    bufferedInputStream.close();
+                }
+            } catch (IOException exception) {
+                System.out.println("Error in InputStream close(): " + exception);
+                exception.printStackTrace();
+            }
+        }
     }
 }
