cfd85003ec206a6d823a34c541166bd8190b77af
vfa.giangnt <giangnt@vitalify.asia>
2020-06-26 11:05:42 +0700

    Java I/O: Create new File

--- src/vitalify/asia/Main.java
+++ src/vitalify/asia/Main.java
@@ -1,10 +1,27 @@
 package vitalify.asia;
 
 import java.io.File;
+import java.io.IOException;
 
 public class Main {
 
+    /*
+    *
+    *  Create a file in Java
+    * */
     public static void main(String[] args) {
-	// write your code here
+        try {
+            File file = new File("./newfile.txt");
+            boolean fvar = file.createNewFile();
+            if (fvar) {
+                System.out.println("File has been created successfully");
+            } else {
+                System.out
+                        .println("File already present at the specific location");
+            }
+        } catch (IOException exception) {
+            System.out.println("Exception Occurred");
+            exception.printStackTrace();
+        }
     }
 }
